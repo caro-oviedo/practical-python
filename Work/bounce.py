9820ab013bc06d8079de573c0a963358ea1e6eb6
caro-oviedo <carolina.u.oviedo@gmail.com>
2020-06-16 21:23:19 -0300

    Add code

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+
+initial_height = int(100) 
+
+
+for i in range(10): 
+      bouncing_height = round (float(3 * initial_height / 5), 4)
+        print(bouncing_height)
+          initial_height = bouncing_height
+            i = i + 1
+ 
+
